965c6d3cfc2f95a6bf770427d4a58409ac57dcf0
JesusGG03 <jgeronimo2704@gmail.com>
2024-04-18 13:02:53 -0400

    feat: accounts and customers tables added

---
+++ Group_Project/api/models/account.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+class Account(Base):
+    __tablename__ = "accounts"
+
+    account_username = Column(String(100), ForeignKey("customers.email"), primary_key=True, index=True)
+    account_password = Column(String(100), index=True)
+    account_name = Column(String(100), ForeignKey("customers.name"))
+    account_address = Column(String(100), ForeignKey("customers.address"))
+    account_phone = Column(String(100), ForeignKey("customers.phone"))
+
+    customers = relationship("Customer", back_populates="accounts")
--- Group_Project/api/models/model_loader.py
+++ Group_Project/api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, account, customerInfo
 
 from ..dependencies.database import engine
 
@@ -9,3 +9,5 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    customerInfo.Base.metadata.create_all(engine)
+    account.Base.metadata.create_all(engine)
